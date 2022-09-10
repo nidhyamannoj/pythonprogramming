dd06b3a14cb0fa666cb4c4885d3b72c0da18afa2
unknown <nidhya.mannoj@gmail.com>
2022-09-10 16:30:00 +0530

    use of placeholder

---
+++ userinput.py
@@ -0,0 +1,9 @@
+name=input("enter a name: ")
+mailid=input("enter a mail id: ")
+ph=input("enter a phone number: ")
+mes="*"
+print(mes * 50)
+print('"UserName : "{}\n"Email: "{}\n "Ph: "{}'.format(name,mailid,ph))
+print(mes * 50)
+
+
